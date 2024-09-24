d0f2b8dda5356873bc24e52f48c288fea6820a20
Razzo_A_Vela <giumifed@gmail.com>
2024-09-24 17:36:57 +0200

    Updated main file

--- src/Smooth.cpp
+++ src/Smooth.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
-#include <TOML/TOML.hpp>
 #include <util/StringUtils.hpp>
+#include <TOML/TOML.hpp>
+#include <PreTokenizer/PreTokenizer.hpp>
+#include <Tokenizer/Tokenizer.hpp>
 
 const std::string configTOMLName = "smoothConfig.toml";
 const std::string defaultConfigTOML = "\n\
 # all paths are relative to smooth.exe\n\
-# main file path\n\
+# main file(.smt) path (without extension)\n\
 mainFile = \"\"\n\
 # out file path (without extension)\n\
 outFile = \"\"\n\
@@ -16,16 +18,36 @@ genAssembly = false\n\
 # should assemble and link (genAssembly required)\n\
 noLink = false\n\
 \n\
-# path for files to be included\n\
-includes = []\n\
-\n\
 # arguments for gcc assembler and linker (ex. [\"-nostdlib\", \"-lSDL_2\"])\n\
 gccArgs = []\n\
 ";
 
 int main(int argc, char* argv[]) {
+  std::cout << "\nReading config...\n";
   TOML::File config(configTOMLName);
   config.init(defaultConfigTOML);
-  std::cout << "Printing...\n";
+  std::cout << "\nPrinting config...\n";
   config.print();
+  std::string mainFile = std::string((char*) config.getContent("mainFile")->getPointer());
+  std::string outFile = std::string((char*) config.getContent("outFile")->getPointer());
+  bool genAssembly = *((bool*) config.getContent("genAssembly")->getPointer());
+  bool noLink = *((bool*) config.getContent("noLink")->getPointer());
+
+  if (!Utils::fileExists(mainFile + ".smt"))
+    Utils::error("File error", "File does not exist");
+
+  std::cout << "\nReading file...\n";
+  std::string file = Utils::readEntireFile(mainFile + ".smt");
+
+  std::cout << "\nPreTokenizing...\n";
+  PreTokenizer::PreTokenizer preTokenizer(file);
+  preTokenizer.process();
+  std::cout << "\nPrinting PreTokens...\n";
+  preTokenizer.print();
+
+  std::cout << "\nTokenizing...\n";
+  Tokenizer::Tokenizer tokenizer(preTokenizer.getOutput());
+  tokenizer.process();
+  std::cout << "\nPrinting Tokens...\n";
+  tokenizer.print();
 }
