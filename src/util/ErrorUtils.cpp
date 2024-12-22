eb50140f4a1b3b471fee669791a8a480021a1aa3
Razzo_A_Vela <giumifed@gmail.com>
2024-12-22 10:23:19 +0100

    Added errorFileName

--- src/util/ErrorUtils.cpp
+++ src/util/ErrorUtils.cpp
@@ -1,10 +1,27 @@
 #include "ErrorUtils.hpp"
 
 namespace Utils {
+  std::string errorFileName = "";
+
+  void setErrorFileName(std::string toSet) {
+    errorFileName = toSet;
+  }
+
+  std::string getErrorFileName() {
+    return errorFileName;
+  }
+
+  void resetErrorFileName() {
+    errorFileName = "";
+  }
+
+
   void error(std::string errType, std::string msg, int line) {
     std::cout << errType << ": " << msg;
     if (line != -1)
       std::cout << " on line: " << line;
+    if (errorFileName != "")
+      std::cout << " in file: \"" << errorFileName << '\"';
     std::cout << '\n';
     exit(1);
   }
--- src/util/ErrorUtils.hpp
+++ src/util/ErrorUtils.hpp
@@ -2,6 +2,10 @@
 #include <string>
 
 namespace Utils {
+  void setErrorFileName(std::string toSet);
+  std::string getErrorFileName();
+  void resetErrorFileName();
+
   [[noreturn]] void error(std::string errType, std::string msg, int line);
   [[noreturn]] void error(std::string errType, std::string msg);
   [[noreturn]] void error(std::string msg, int line);
