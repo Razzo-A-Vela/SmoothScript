9fba42f6322d703ac5b24a36868136c685cb798e
Razzo_A_Vela <giumifed@gmail.com>
2025-03-28 19:00:46 +0100

    Fixed some bugs

--- src/util/VectorUtils.hpp
+++ src/util/VectorUtils.hpp
@@ -4,33 +4,12 @@
 
 namespace Utils {
   template<typename T>
-  std::vector<T> insertInIndex(std::vector<T> toInsert, int index, std::function<void(std::vector<T>*)> addFunc) {
-    std::vector<T> ret;
-
-    for (int i = 0; i < toInsert.size(); i++) {
-      if (i == index)
-        addFunc(&ret);
-      ret.push_back(toInsert.at(i));
-    }
-
-    return ret;
+  void insertInIndex(std::vector<T>& toInsert, int index, T toAdd) {
+    toInsert.insert(toInsert.begin() + index, toAdd);
   }
 
   template<typename T>
-  std::vector<T> insertInIndex(std::vector<T> toInsert, int index, T toAdd) {
-    std::vector<T> ret;
-
-    for (int i = 0; i < toInsert.size(); i++) {
-      if (i == index)
-        ret.push_back(toAdd);
-      ret.push_back(toInsert.at(i));
-    }
-
-    return ret;
-  }
-
-  template<typename T>
-  int getVectorIndex(std::vector<T> vec, T toCheck, std::function<bool(T, T)> equalFunc) {
+  int getVectorIndex(std::vector<T>& vec, T toCheck, std::function<bool(T, T)> equalFunc) {
     for (int i = 0; i < vec.size(); i++) {
       if (equalFunc(vec.at(i), toCheck))
         return i;
@@ -39,7 +18,7 @@ namespace Utils {
   }
 
   template<typename T>
-  int getVectorIndex(std::vector<T> vec, T toCheck) {
+  int getVectorIndex(std::vector<T>& vec, T toCheck) {
     for (int i = 0; i < vec.size(); i++) {
       if (vec.at(i) == toCheck)
         return i;
@@ -48,12 +27,12 @@ namespace Utils {
   }
 
   template<typename T>
-  bool isInVector(std::vector<T> vec, T toCheck, std::function<bool(T, T)> equalFunc) {
+  bool isInVector(std::vector<T>& vec, T toCheck, std::function<bool(T, T)> equalFunc) {
     return getVectorIndex(vec, toCheck, equalFunc) != -1;
   }
 
   template<typename T>
-  bool isInVector(std::vector<T> vec, T toCheck) {
+  bool isInVector(std::vector<T>& vec, T toCheck) {
     return getVectorIndex(vec, toCheck) != -1;
   }
 }
