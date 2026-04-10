ca9fdfcfbfc79909385de21d6a040cd6dc77e019
Razzo_A_Vela <giumifed@gmail.com>
2026-04-11 01:26:48 +0200

    Added the implementation of the new syntax (only variables, will add more)

--- src/Parser/SyntaxChecker.cpp
+++ src/Parser/SyntaxChecker.cpp
@@ -1,8 +1,77 @@
 #include "SyntaxChecker.hpp"
 
 namespace Parser {
+  Utils::Error SyntaxChecker::syntaxError(const char* msg) {
+    return Parser::syntaxError(msg, getErrorLine());
+  }
+
+  int SyntaxChecker::getErrorLine() {
+    if (hasPeek())
+      return peekValue().line;
+    else if (hasPeek(-1))
+      return peekValue(-1).line;
+    else
+      return 1; //* Technically it should be unreachable
+  }
+
+  bool SyntaxChecker::wakeup(Token token, TokenType tokenType) {
+    return token == Token{ tokenType };
+  }
+
+  bool SyntaxChecker::wakeup(TokenType tokenType) {
+    return tryConsume({ tokenType });
+  }
+
+
+  #define expectError(retType, type, result, function) \
+    Result::inst<type> result##_result = function; \
+    if (!result##_result.hasValue()) \
+      return Result::error<retType>(result##_result.error); \
+    result = result##_result.value
+  
+  #define expectIgnore(retType, type, result, function) \
+    Result::inst<type> result##_result = function; \
+    if (!result##_result.hasValue()) \
+      return Result::ignore<retType>(result##_result.error); \
+    result = result##_result.value
+
+
+  Result::inst<Variable> SyntaxChecker::processVariable() {
+    Type* type;
+    Identifier* name;
+    expectError(Variable, Type, type, processType());
+    expectError(Variable, Identifier, name, processIdentifier());
+    return Result::success(new Variable{ type, name, NULL });
+  }
+
+  Result::inst<Identifier> SyntaxChecker::processIdentifier() {
+    if (peekEqual({ TokenType::IDENTIFIER }))
+      return Result::success(new Identifier{ consume().value().u.string });
+    return Result::ignore<Identifier>(syntaxError("Expected identifier"));
+  }
+
+  Result::inst<Type> SyntaxChecker::processType() {
+    if (tryConsume({ TokenType::INT }))
+      return Result::success(new Type{ Type::TypeT::INT });
+    return Result::ignore<Type>(syntaxError("Expected type"));
+  }
+
+
+  #undef expectError
+  #undef expectIgnore
+
+
   void SyntaxChecker::process() {
+    while(hasPeek()) {
+      Token token = consume().value();
       
+      if (wakeup(token, TokenType::SEMI))
+        continue; //* Technically not needed (better than ';')
+      else if (wakeup(token, TokenType::COLON))
+        addToOutput({ GlobalNode::Type::VAR, { .var = expectSemi(processVariable()).expect() } });
+      else
+        Utils::error(syntaxError("Unexpected token"));
+    }
   }
 
   void SyntaxChecker::print(std::ostream& out) {
--- src/Parser/SyntaxChecker.hpp
+++ src/Parser/SyntaxChecker.hpp
@@ -17,6 +17,21 @@ namespace Parser {
     SyntaxChecker(std::vector<Token> tokens) : Processor(tokens.size()), tokens(tokens) {}
     virtual void process();
     virtual void print(std::ostream& out);
+    [[nodiscard]] Utils::Error syntaxError(const char* msg);
+    int getErrorLine();
+    //? A wakeup token is a disposable token that is used to indicate the start of a specific syntax
+    bool wakeup(Token token, TokenType tokenType);
+    bool wakeup(TokenType tokenType);
+    Result::inst<Variable> processVariable();
+    Result::inst<Identifier> processIdentifier();
+    Result::inst<Type> processType();
+    
+    template <typename T>
+    Result::inst<T> expectSemi(Result::inst<T> other) {
+      if (!wakeup(TokenType::SEMI))
+        return Result::error<T>(syntaxError("Expected ';'"));
+      return other;
+    }
 
   protected:
     virtual Token get(int index) { return tokens.at(index); }
